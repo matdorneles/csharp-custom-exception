7dcb1382421c0f52e433abaef53f970fa41dc199
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2023-04-19 15:33:53 -0300

    Add project files.

---
+++ Course/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Course.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using Course.Entities;
+using Course.Entities.Exceptions;
+
+namespace Aulas
+{
+    class Aulas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data:");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Account acc = new Account(number, holder, balance, withdrawLimit);
+
+            try
+            {
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.Withdraw(amount);
+                Console.Write("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine($"Withdraw error: " + e.Message);
+            }
+            
+        }
+    }
+}
